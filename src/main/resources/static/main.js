056c5baeeaca1fae7ed1d7424ffaf1d45b8444ce
Your Name <leonoradli94@gmail.com>
2021-05-12 14:15:48 +0200

    js en proceso

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,10 @@
+let listaElementos = ["proebas lista"];
+
+
+function addElement (){
+    mostrarLista.innerHTML = listaElementos["hola"];
+    const mostrarLista = document.querySelector("#nuestralistadetareas");
+}
+function deleteElement(){
+
+}
