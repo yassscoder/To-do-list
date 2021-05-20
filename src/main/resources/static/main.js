6b55aac26bdd376a8cc5fc1ce997311c53c8e430
yassscoder <yasminlcg@gmail.com>
2021-05-20 11:05:17 +0200

    changing js function reload

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,11 +1,25 @@
 const toDoList = document.getElementById("todo-list");
 const addTask = document.getElementById("addButton");
-let inputField = document.getElementById("write");
+//let inputField = document.getElementById("write");
 
-function addList(){
-    let tareas = inputField.value;
-    toDoList.innerHTML = tareas;
-}
+//function addList(){
+//    let tareas = inputField.value;
+//    toDoList.innerHTML = tareas;
+
+//}
 
 addTask.addEventListener("click", addList);
 
+function showTasks(tasks){
+    const htmlList = document.createElement("ul");
+    htmlList.innerHTML =
+
+
+}
+let reloadTasks = () =>{
+    fetch("/blocks")
+        .then(r => r.json())
+        .then(showTasks)
+};
+
+document.addEventListener("DOMContentLoaded", reloadTasks)
