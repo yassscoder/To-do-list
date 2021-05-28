e90415d06af87589fdd1475dd3c531b6e48e7f24
yassscoder <yasminlcg@gmail.com>
2021-05-28 09:39:28 +0200

    function validation js

---
+++ src/main/java/com/example/todolist/models/TaskHashmap.java
@@ -0,0 +1,2 @@
+package com.example.todolist.models;public class TaskHashmap {
+}
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,36 +1,37 @@
 
-
-//    toDoList.innerHTML = tareas;
-//}
-//addTask.addEventListener("click", addList);
-//let tareas = inputField.value;
-
-//${task.write_task}
 const toDoList = document.getElementById("todo-list");
 const btn =document.getElementById("addButton");
 
 function showTasks(tasks) {
     toDoList.innerHTML="";
-
         for(let [location, task] of Object.entries(tasks)){
-        if (task.name.length>2) {
-            const new_div = document.createElement("div");
-            new_div.innerHTML = `<div class="task">
-                <div 
-                class="close-button" 
-                onclick="fetch(\` /blocks/${location}\`,
-                { method: 'DELETE'}).then(reloadTasks)"><div class="task-button"></div></div>
-              <p class="task_name">${task.name}</p>
-           </div>`;
 
-            toDoList.appendChild(new_div)
+                const new_div = document.createElement("div");
+                new_div.innerHTML = `<div class="task">
+                    <div class="close-button" onclick="fetch(\` /blocks/${location}\`,
+                { method: 'DELETE'}).then(reloadTasks)"><div class="task-button"></div></div>
+                    <p class="task_name">${task.name}</p>
+                </div>`;
+
+                 toDoList.appendChild(new_div)
+
+
         }
-        else {
-                //alert("Has de poner al menos 3 caracteres");
-        }
+    }
+
+const addedTask=document.getElementById("name");
+function validation() {
+    const inputField = addedTask.value;
+    if (inputField.length>2){
+        btn.disabled = false;
+    }
+    else {
+        btn.disabled = true;
 
     }
 }
+addedTask.addEventListener("change", validation);
+//se puede usar "input"/"change"
 
 let reloadTasks = () =>{
     fetch("/blocks")
